e30878ba297012bbd107e0db1861e865e9aa5617
Volha Sakharevich <vuictorovna@gmail.com>
2023-08-24 21:26:38 +0100

    Improve webhook message

--- webhook.py
+++ webhook.py
@@ -2,18 +2,17 @@ import requests
 
 import os
 from dotenv import load_dotenv
-import json
+
 
 load_dotenv()
 
 
 def send_ifttt_webhook(event, symbol, current_price, previous_price):
     webhook_key = os.getenv("WEBHOOK_KEY")
-    url = f"https://maker.ifttt.com/trigger/{event}/json/with/key/{webhook_key}"
-    headers = {"Content-Type": "application/json"}
+    url = f"https://maker.ifttt.com/trigger/{event}/with/key/{webhook_key}"
     payload = {
-        "symbol": symbol,
-        "previous_price": str(previous_price),
-        "current_price": str(current_price),
+        "value1": symbol,
+        "value2": str("{:.2f}".format(round(current_price, 2))),
+        "value3": str("{:.2f}".format(round(previous_price, 2))),
     }
-    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    response = requests.post(url, json=payload)
